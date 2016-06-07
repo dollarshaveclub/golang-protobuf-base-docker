206ce9899a1cc520ba8a7c7d4b29bcc935ca6544
Benjamen Keroack <benjamen.keroack@dollarshaveclub.com>
2016-06-07 11:23:42 -0700

    initial version

---
+++ README.md
@@ -0,0 +1,2 @@
+Go Base Image with Protobuf 3 (and gRPC)
+========================================
---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+PACKAGES="git build-base autoconf automake libtool"
+
+apk add --update $PACKAGES
+
+git clone https://github.com/google/protobuf -b $PROTOBUF_TAG --depth 1
+
+cd ./protobuf
+
+./autogen.sh && \
+  ./configure --prefix=/usr && \
+  make -j 3 && \
+  make check && \
+  make install
+
+cd ..
+rm -rf ./protobuf
+
+apk add --update libstdc++
+
+go get -u -v github.com/golang/protobuf/proto
+go get -u -v github.com/golang/protobuf/protoc-gen-go
+go get -u -v google.golang.org/grpc
+
+apk del $PACKAGES
+rm -rf /var/cache/apk/*
